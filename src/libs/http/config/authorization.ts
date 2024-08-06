210a6a4b8be1630ecd2ad977007b35d401bda7f5
Myers <33033276+jiezhuCl@users.noreply.github.com>
2024-08-06 14:36:27 +0800

    前端代码提交

--- src/libs/http/config/authorization.ts
+++ src/libs/http/config/authorization.ts
@@ -21,7 +21,7 @@ const Authorization = function(cookieName: string, tokenKey?: string) {
   const headers = {};
   if (value) {
     const key = tokenKey || "token";
-    Object.assign(headers, { [key]: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJlaWQiOiIwMDAwMHJhMnp4bTUwc3BjNnEzMXdqOW9iZ2Z5NzRuZCIsInVzZXJfbmFtZSI6InJvb3QiLCJ0eiI6Ik1hcnF1ZXNhcyBTdGFuZGFyZCBUaW1lIiwicGlkIjotMSwib2lkIjotMSwib2xkaWQiOm51bGwsImNsaWVudF9pZCI6ImxxYV9nYW1lIiwiYXR0YWNocyI6IiIsImVuYW1lIjoiTFFBIiwiaHBpZCI6bnVsbCwiYXR5cGUiOiIxIiwic2NvcGUiOlsiYWxsIl0sImRuYW1lIjoiRUNJW-i2heeuoV0iLCJleHAiOjE3MjMwMTM4OTYsInF5d3giOm51bGwsImFpZCI6IjAwMDAwMDA1eHY2ZTkwb3RkbjdobWYyYjR1c3pqcGljIiwianRpIjoiOWYyNzFkMmYtNTYyOS00OTg2LWExOTQtMjEzZjBjMTFkNzA2IiwiZGlkIjotMX0.gUzAMkSXxQgjSHpCivyEDhkfSg-G0bJQhfLWNTFfpci9X2S5bHH4NPTek_TkksGsRFLtaU55Ue61PIsABAE0ZfYy_5p5mKwlbODCL6WvIwBZzY4b-ph-yCB4pFleOdVzw51EoYesgAk-90w5GPc56eb3VhhF6at6Hp0PX_AFrV7i8_HQ5JG6Qbz0-ozSqKihJNQovyUcJw3XH5anrTXV_qctq3zYHR_TEMrMs0lC2bfXGqT9q735a-8w8q8ZyMTwro6vUM9u_wETbsvVO-B0A1doxsuaVmeMbfovYwiSh5LObaPfe9X0WUsk4U9ZtCYL3VjUn07KNUv9ot2Cec9CAQ` });
+    Object.assign(headers, { [key]: `Bearer ${value}` });
   }
   return headers;
 }
