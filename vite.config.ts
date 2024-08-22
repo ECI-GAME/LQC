d6a177479e522b06be7236b8f179e176a6274431
svonme <svon.me@gmail.com>
2024-08-22 14:44:47 +0800

    feat: vite.config 配置调整

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,25 @@ import commonjs from 'vite-plugin-commonjs';
 import htmlPlugin from "vite-plugin-html-config";
 // @ts-ignore
 import ElementPlus from 'unplugin-element-plus/vite';
+import safeGet from "@fengqiaogang/safe-get";
 
-const Config = defineConfig(function ({mode}: UserConfig) {
-  const env = loadEnv(mode || "production", "./");
+const Config = defineConfig(function (config: UserConfig) {
+  const env = loadEnv(config.mode || "production", "./");
   const headScripts = [];
+  const alias: { [key: string]: string } = {};
   // 引入 iconfont
   if (env.VITE_APP_ICONFONT) {
     headScripts.push({defer: true, src: env.VITE_APP_ICONFONT});
   }
-
+  const command = safeGet<string>(config, "command");
+  if (command !== "build") {
+    alias["vue"] = path.resolve("node_modules", "vue/dist", "vue.esm-browser.js");
+  }
   return {
     resolve: {
       extensions: [".ts", ".vue", ".cjs", ".js", ".tsx"],
       alias: {
+        ...alias,
         "src/": `${path.resolve(__dirname, "src")}/`,
         "types/": `${path.resolve(__dirname, "types")}/`,
         "@ue/http": path.resolve(__dirname, "src/libs/http"),
@@ -30,7 +36,7 @@ const Config = defineConfig(function ({mode}: UserConfig) {
       vue(),
       jsx(),
       htmlPlugin({headScripts}),
-      ElementPlus()
+      ElementPlus({})
     ],
     server: {
       port: 9090,
